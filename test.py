a1b01c8e7b975d4a499a1138af6fdb2363938f19
Md.Baizeed <imaginaryrony@gmail.com>
2019-07-06 17:54:45 +0600

    test added

---
+++ test.py
@@ -0,0 +1,24 @@
+'''class Rectangel():
+    def __init__(self,w,h):
+        self.w = w
+        self.h = h
+
+    def area(self):
+        self.w*self.h
+
+    def parimeter(self):
+        return 2 * (self.w+self.h)
+
+
+class square(Rectangel):'''
+
+
+mytupple = ('mango','apple','banana','lichi','jackfruit','goava')
+
+myit = iter(mytupple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
