418d3a4d94b7eac3f0ba5d9f57d0870eec449ade
abc <baizeedrony@gmail.com>
2021-10-26 13:26:43 +0600

    update

--- app.py
+++ app.py
@@ -65,6 +65,10 @@ class RegistrationForm(Form):
 @app.route('/')
 def index():
     return render_template("home.html")
+#home2
+@app.route('/')
+def home2():
+    return render_template("home2.html")
 
 # About
 @app.route('/about')
